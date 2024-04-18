a82015273638de12024b842773f0e047a4d2f611
PinappleUnderTheSea <1411493875@qq.com>
2024-04-18 23:45:26 +0800

    change get string with 2 strings

--- examples/plugin/get-string/getstring.cpp
+++ examples/plugin/get-string/getstring.cpp
@@ -13,21 +13,25 @@ namespace {
 using namespace std::literals::string_view_literals;
 using namespace WasmEdge;
 
-PO::Option<std::string> StringOpt(PO::Description("string to return"sv),
-                                  PO::MetaVar("STRING"sv),
-                                  PO::DefaultValue<std::string>("hello"));
+PO::Option<std::string> StringOpt1(PO::Description("string to return"sv),
+                                  PO::MetaVar("STRING1"sv),
+                                  PO::DefaultValue<std::string>("hello1"));
+
+PO::Option<std::string> StringOpt2(PO::Description("string to return2"sv),
+                                  PO::MetaVar("STRIN2G"sv),
+                                  PO::DefaultValue<std::string>("world"));
 
 PO::Option<PO::Toggle> UpperOpt(PO::Description("return in upper case"sv));
 
 void addOptions(const Plugin::Plugin::PluginDescriptor *,
                 PO::ArgumentParser &Parser) noexcept {
-  Parser.add_option("string"sv, StringOpt).add_option("upper"sv, UpperOpt);
+  Parser.add_option("string1"sv, StringOpt1).add_option("string2", StringOpt2).add_option("upper"sv, UpperOpt);
 }
 
 class GetString : public Runtime::HostFunction<GetString> {
 public:
-  GetString(const std::string &String, bool Upper)
-      : String(String), Upper(Upper) {}
+  GetString(const std::string &String1, const std::string String2, bool Upper)
+      : String1(String1), String2(String2), Upper(Upper) {}
   Expect<void> body(const Runtime::CallingFrame &Frame, uint32_t BufPtr,
                     uint32_t BufLen, uint32_t WrittenPtr) {
     // Check memory instance from module.
@@ -47,29 +51,28 @@ public:
     if (unlikely(Written == nullptr)) {
       return Unexpect(ErrCode::Value::HostFuncError);
     }
-
-    if (Upper) {
-      char *const End =
-          std::transform(String.begin(), String.end(), Buf.data(),
-                         [](unsigned char C) { return std::toupper(C); });
+    if(Upper){
+      char *const End = std::copy(String1.begin(), String1.end(), Buf.data());
       *Written = End - Buf.data();
-    } else {
-      char *const End = std::copy(String.begin(), String.end(), Buf.data());
+    }else{
+      char *const End = std::copy(String2.begin(), String2.end(), Buf.data());
       *Written = End - Buf.data();
     }
-//    std::cout << *Written<<std::endl;
+    //    std::cout << *Written<<std::endl;
     return {};
   }
 
 private:
-  std::string_view String;
+  std::string_view String1;
+  std::string_view  String2;
   bool Upper;
 };
 
 class PluginModule : public Runtime::Instance::ModuleInstance {
 public:
   PluginModule() : ModuleInstance("module_wasm_name") {
-    addHostFunc("get_string", std::make_unique<GetString>(StringOpt.value(),
+    addHostFunc("get_string", std::make_unique<GetString>(StringOpt1.value(),
+                                                          StringOpt2.value(),
                                                           UpperOpt.value()));
   }
 };
